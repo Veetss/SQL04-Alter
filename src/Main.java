b4ecfdccd3c603effa2788d396be534cfd6d0499
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-14 12:14:45 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -48,10 +48,10 @@ public class Main {
 
             s.executeUpdate(sql1);
 
-            s.executeUpdate("UPDATE student SET country = \"Italy\" where last_name = \"Boukoti\";");
-            s.executeUpdate("UPDATE student SET country = \"Italy\" where last_name = \"Vassarotti\";");
-            s.executeUpdate("UPDATE student SET country = \"Germany\" where last_name = \"De Fenzo\";");
-            s.executeUpdate("UPDATE student SET country = \"Germany\" where last_name = \"Mustata\";");
+            s.executeUpdate("UPDATE student SET country = \"Italy\" where student_id = 0;");
+            s.executeUpdate("UPDATE student SET country = \"Italy\" where student_id = 1;");
+            s.executeUpdate("UPDATE student SET country = \"Germany\" where student_id = 2;");
+            s.executeUpdate("UPDATE student SET country = \"Germany\" where student_id = 3;");
 
         } catch(SQLException e) {
             System.out.println(e.getMessage());
